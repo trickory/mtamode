f42e75525d7f7c977475ca638fbfbf847785f378
trickory <trickory@gmx.de>
2017-02-04 21:27:13 +0100

    Added accounts

--- hack.lua
+++ hack.lua
@@ -1,9 +1,7 @@
 local timer = false                                                             -- variable to check if timer is running (cooldown for atm's)
 
 function spawn()
-	spawnPlayer(source, -2246, 2372, 5);
-	setPlayerMoney(source, 1000)
-	giveWeapon(source, 22, 34)
+	spawnPlayer(source, -2246, 2372, 5)
 	fadeCamera(source, true)
 	setCameraTarget(source, source)
 	showChat(source, true)
@@ -15,29 +13,78 @@ function hackATM(player, atm)
 
 	local win = math.random(500, 2500)
 
-	for i, v in pairs(atms) do                                                    -- for loop to check if player pos matches one of the atms pos
-		local posx = getElementData(v, "posX")                                      -- get "PosX" of the arraypos v
-		local posy = getElementData(v, "posY")                                      -- get "PosY" of the arraypos v
-		local size = getElementData(v, "size")                                      -- get "size" of the arraypos v
+	for i, v in pairs(atms) do                                                     --// for-loop to check        \\--
+		local posx = getElementData(v, "posX")                                       --|| if players pos matches   ||--
+		local posy = getElementData(v, "posY")                                       --\\ with one of the atms pos //--
+		local size = getElementData(v, "size")
 
-    local pos1, pos2 = playerx < posx + size, playerx > posx - size             -- comparing x-coordinates
-    local pos3, pos4 = playery < posy + size, playery > posy - size             -- comparing y-coordinates
+    local pos1, pos2 = playerx < posx + size, playerx > posx - size
+    local pos3, pos4 = playery < posy + size, playery > posy - size
 
-		if (pos1 and pos2 and pos3 and pos4 and timer == false) then                -- if comparisons are true and timer isnt running, proceed
-			outputChatBox("Hacked and earned: "..win.."$", player)                    -- chatbox output of money earned to player only
-			givePlayerMoney(player, win)                                              -- gives won amount to player
-			setTimer(                                                                 --[[ starts a timer to run for 15 seconds and sets timer to false afterwards ]]--
-      function()
+		if (pos1 and pos2 and pos3 and pos4 and timer == false) then
+			outputChatBox("Hacked and earned: "..win.."$", player)
+			givePlayerMoney(player, win)
+
+			setTimer(function()
 				outputChatBox("ATM's hackable again!")
 				timer = false;
 			end ,15000,1)
-			timer = true                                                              -- sets timer to true after timer has started
+
+			timer = true
 		end
 	end
 end
 
+local register = false
+
+function registerPlayer ( source, commandName, name, password )
+	if ( password ~= "" and password ~= nil and register == false) then
+		local accountAdded = addAccount( name, password )
+		if ( accountAdded ) then
+			outputChatBox ( "Thank you " .. getPlayerName(source) .. ", you're now registed, you can login with /login", source )
+			register = true
+			exports.inventory:addPlayerItem(source, "SilencedPistol")
+			exports.inventory:addPlayerItem(source, "PistolM")
+		else
+			outputChatBox ( "Error creating account, contact the server admin", source )
+		end
+	else
+		outputChatBox ("Error creating account, correct syntax: /register <loginname> <password>", source )
+	end
+end
+
+function onPlayerLog(_, acc)
+	if (acc) then
+		local moneylog = getAccountData(acc, "playercash")
+		if (moneylog) then
+			setPlayerMoney(source, moneylog)
+		end
+	end
+end
+
+function onPlayerLogO()
+	setPlayerMoney(source, 0)
+end
+
+function onPlayerQ()
+	local acc = getPlayerAccount(source)
+	if (acc) and not isGuestAccount(acc) then
+		local money = getPlayerMoney(source)
+		setAccountData(acc, "playercash", money)
+	end
+end
+
+addEventHandler("onPlayerLogin" , root, onPlayerLog)
+addEventHandler("onPlayerQuit", root, onPlayerQ)
+addEventHandler("onPlayerLogout", root, onPlayerLogO)
+
 addEventHandler("onPlayerJoin", root, spawn)
 
 addCommandHandler("hack", hackATM)
+addCommandHandler ("reg", registerPlayer)
 
-addEventHandler("onPlayerWasted", root)
+addEventHandler("onPlayerWasted", root,
+	function()
+		setTimer(spawn, 1800, 1, source)
+	end
+)
